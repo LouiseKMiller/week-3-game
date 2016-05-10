5d40101795ebb3a5e98830f703f4cba45f3af6f1
Louise Miller <louise.austex@gmail.com>
2016-05-10 10:47:30 -0500

    working except determining win when there is a space

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,12 @@
 
-var test = "test";
 var hmgame = {
-	words : ["transporter", "nebula", "photon torpedos", "klingon", "enterprise", "jeffries tube", "spacial anomaly", "dilithium", "tricorder", "stardate", "prime directive", "cloaking device", "holodeck", "tribble", "borg collective", "shuttlecraft"],
-	triesSoFar : 0,
+	words : ["test", "photon torpedo", "transporter", "nebula", "klingon", "enterprise", "jeffries tube", "spacial anomaly", "dilithium", "tricorder", "stardate", "prime directive", "cloaking device", "holodeck", "tribble", "borg collective", "shuttlecraft"],
+	triesRemain : 12,
 	allGuesses : [],
 	userWordArray : [],
+	userWordString : "",
 	roundNum : 0,
+	winCount : 0,
 
 	currentWord : function () {
 			  return (this.words[this.roundNum]);
@@ -19,33 +20,42 @@ var hmgame = {
 				return ((this.allGuesses.includes(userGuess) == false) && (userGuess.charCodeAt(0) > 96) && (userGuess.charCodeAt(0) < 123));
 				},
 
-// 	createArray : function (argArray) {
-//				var argArray = [];
-//				for (var i=0; (i<argString.length); i++) {
-//					argArray[i] = argString.charAt(i);
-//				}
-//				return argArray;
-//				},
-//
-// updates userWordArray with correct guess
-//
 	updateWord	: function () {
 				var localWord = this.currentWord();
-				console.log(localWord);
 				for (var i=0; (i < localWord.length); i++) {
 					if (userGuess.charCodeAt(0) == localWord.charCodeAt(i))
 						this.userWordArray[i]=userGuess;
 					}
+				},
+// updates userWordArray with correct guess
+//
+	displayWord	: function (localTag,localString) {
+				var htmlWord1 = (localString);
+				document.querySelector(localTag).innerHTML = htmlWord1;
 				},
 //
 // initializes userWord to match length of currentWord, insert spaces, and replace letters with underscores
 //
 	initUserWord : function() {
 				for (var i=0; (i < this.currentWord().length); i++) {
-					if (this.currentWord().charCodeAt[i]==32) this.userWordArray[i] = " ";
+					if (this.currentWord().charCodeAt(i)==32) this.userWordArray[i] = "&nbsp";
 					else this.userWordArray[i]= "_";
 					}
 				return this.userWordArray;
 				},
+//
+// starts new game
+//
+	newGame : function() {
+			this.roundNum++;
+			this.triesRemain = 12;
+			this.displayWord('#triesLeft', this.triesRemain);
+			this.allGuesses = [];
+			this.displayWord('#guessList', 'None Yet!');
+			this.userWordArray = [];
+			this.userWordArray = this.initUserWord();
+			this.displayWord('#word', this.userWordArray.join(" "));
+
+	}
 
 }
